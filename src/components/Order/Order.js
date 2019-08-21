5d3042a2645d72a4a4c704150040fdf8d39a5778
Felipe Pina <fop.net@gmail.com>
2019-08-21 18:06:45 -0300

    Orders and Order Component

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,26 @@ import React from "react";
 import classes from "./Order.css";
 
 const order = props => {
+  const summary = Object.keys(props.order.ingredients).map((key, i) => (
+    <span
+      style={{
+        textTransform: "capitalize",
+        display: "inline-block",
+        margin: "0px 8px",
+        border: "1px solid #ccc",
+        padding: "5px",
+      }}
+    >
+      {key} ({props.order.ingredients[key]})
+    </span>
+  ));
+
   return (
     <div className={classes.Order}>
-      <p>Ingredients: Salad (1)</p>
+      <p>Ingredients: </p>
+      {summary}
       <p>
-        Price <strong>$ 3.00</strong>
+        Price: <strong>$ {props.order.totalPrice.toFixed(2)}</strong>
       </p>
     </div>
   );
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,15 +1,43 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import Order from "../../components/Order/Order";
+import axios from "../../axios-order";
+import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "../../hoc/withErrorHandler/withErroHandler";
 
 class Orders extends Component {
-    render() { 
-        return (
-            <div>
-                <Order />
-            </div>
-        );
-    };
+  state = {
+    orders: [],
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    axios
+      .get("/orders.json")
+      .then(resp => {
+        const orders = [];
+        for (let key in resp.data) {
+          orders.push({
+            ...resp.data[key],
+            id: key,
+          });
+        }
+        this.setState({ orders: orders });
+      })
+      .finally(() => this.setState({ loading: false }));
+  }
+
+  render() {
+    const orders = this.state.loading ? (
+      <Spinner />
+    ) : this.state.orders.length ? (
+      this.state.orders.map((o, i) => <Order key={o.id} order={o} />)
+    ) : (
+      <h2>Ops, no order has been found.</h2>
+    );
+
+    return <div>{orders}</div>;
+  }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
